07f90627762086f8f85fdfad7fca0b2bda7588c5
Dave Pitts <dgapitss@gmail.com>
2018-10-11 23:30:47 +0200

    remove code still in development

--- test/ebabel-theme-music.test.js
+++ test/ebabel-theme-music.test.js
@@ -5,13 +5,13 @@ const themeMusic = require('../src/ebabel-theme-music.js');
 
 let THREE;
 let camera;
-let sound;
-let listener;
+//let sound;
+//let listener;
 
 beforeEach(() => {
   THREE = mockTHREE;
   camera = new THREE.PerspectiveCamera();
-   sound = new THREE.Audio(listener);
+//   sound = new THREE.Audio(listener);
 });
 
 test('themeMusic returns something other than undefined.', () => {
@@ -44,16 +44,16 @@ test('audioLoader returns something other than undefined.', () => {
 
    // Load a sound and set it as the Audio object's buffer.
    const audioLoader = new THREE.AudioLoader();
-   const url='assets/music/ambient2-nautilus.mp3';
-   const loop=false;
-   const volume=0.7;
-   const autostart=true;
-   audioLoader.load(url, (buffer) => {
-     sound.setBuffer(buffer);
-     sound.setLoop(loop);
-     sound.setVolume(volume);
-     if (autostart) sound.play();
-   });
+  //  const url='assets/music/ambient2-nautilus.mp3';
+  //  const loop=false;
+  //  const volume=0.7;
+  //  const autostart=true;
+  //  audioLoader.load(url, (buffer) => {
+  //    sound.setBuffer(buffer);
+  //    sound.setLoop(loop);
+  //    sound.setVolume(volume);
+  //    if (autostart) sound.play();
+  //  });
 
   expect(audioLoader !== undefined).toBe(true);
 
